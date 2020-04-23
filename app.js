7cb1082b7780be222b0a931d4b328e49f3db885b
NAQVI <nomanalam7214@gmail.com>
2020-04-23 12:21:09 +0200

    connection established between app and mongodb

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express= require("express")
 const app = express()
 const createError = require("http-errors")
+const mongoose =require("mongoose")
 
 const indexRoute = require("./routes/indexRoute")
 const recordsRoute =require("./routes/recordsRoute")
 const usersRoute = require("./routes/usersRoute")
 const ordersRoute = require("./routes/ordersRoute")
 
+
 const port = process.env.PORT ||  3000; 
+
+mongoose.connect("mongodb://127.0.0.1:27017/record-shop-live",{ useNewUrlParser: true ,useUnifiedTopology: true })
+mongoose.connection.on("error",(err)=>console.log(err))
+mongoose.connection.on("open", ()=>console.log("database connected"))
 
 
 app.use(express.json())
